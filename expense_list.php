5610fe2406f6147b51fb31e96a2d6040cadd2b5b
Aditya Mohite <mohiteaditya689@gmail.com>
2024-03-04 17:44:20 +0530

    php script handling the display of records

--- expense_list.php
+++ expense_list.php
@@ -3,34 +3,31 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-
-// if (!isset($_SESSION['username'])) {
-//     echo '<div id="norecords"class="alert alert-warning" role="alert">
-//     No Records Founded.
-// </div>';
-// exit();
-// }
-
+// Include database connection
 include "dbconnection.php";
+
 function getUserExpense($conn, $user)
 {
+    // SQL query to fetch expenses for a user
     $stmt = "SELECT * FROM expenses WHERE USER = '$user';";
     $result = mysqli_query($conn, $stmt);
     $totalexpense = 0;
 
     if ($result->num_rows > 0) {
+        // Display expense records in a table
         echo '<table class="table table-striped" border=2>';
         echo '<thead class="thead-dark">';
         echo '<tr><th>Description</th><th>Amount</th><th>Date</th><th>Action</th></tr>';
         echo '</thead>';
         echo '<tbody>';
 
+        // Loop through fetched records and display them
         while ($row = mysqli_fetch_assoc($result)) {
             echo '<tr>';
             echo '<td>' . $row["DESCRIPTION"] . '</td>';
             echo '<td>₹' . $row["AMOUNT"] . '</td>';
             echo '<td>' . $row["TRANSACTION_DATE"] . '</td>';
-            echo '<td><button class="btn btn-primary" onclick="fetchRecordData(' . $row["ID"] . ')"> <span class="glyphicon glyphicon-pencil"> </span> Edit</button>';
+            echo '<td><button class="btn btn-primary" onclick="redirectToEdit(' . $row["ID"] . ')"> <span class="glyphicon glyphicon-pencil"> </span> Edit</button>';
             echo ' <button class="btn btn-danger" onclick="deleteRecord(' . $row["ID"] . ')"> <span class="glyphicon glyphicon-trash"> </span> Delete</button></td>';
             echo '</tr>';
 
@@ -39,10 +36,9 @@ function getUserExpense($conn, $user)
 
         echo '</tbody>';
         echo '</table>';
-        echo "<br><b>Total Expense: ₹" . number_format($totalexpense, 2) . "</b>"; // Print total expense with 2 decimal places
-
-
+        echo "<br><h5>Total Expense: ₹" . number_format($totalexpense, 2) . "</h5>"; // Print total expense with 2 decimal places
     } else {
+        // No records found message
         echo '<div id="norecords"class="alert alert-warning" role="alert">
                             No Records Founded.
                         </div>';
@@ -53,5 +49,4 @@ function getUserExpense($conn, $user)
 
 $user = $_SESSION['username'];
 getUserExpense($conn, $user);
-
 ?>
